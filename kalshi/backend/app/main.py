e0219510789a2c02eeae078d2dcc4185cb7f74a8
Yemi Akomolafe <omopro@yahoo.com>
2026-07-12 14:03:59 -0500

    adding backend, models and database files. particularly adding notes to database file and adding /matches tab to fastapi file

--- kalshi/backend/app/main.py
+++ kalshi/backend/app/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def home():
     return{"message": "Welcome to the Kalshi Ai Dashboard!"}
+
+@app.get("/matches")
+def matches():
+    return{"message": "Today's Matches: Match 1, Match2, Match 3"}
