a9c18adbb79afbea3b34ce44cd2989c3dcb8c09d
Yemi Akomolafe <omopro@yahoo.com>
2026-07-12 17:34:08 -0500

    Update database.py

--- kalshi/backend/app/database.py
+++ kalshi/backend/app/database.py
@@ -7,3 +7,26 @@ NOTICE: important installation - pip install sqlalchemy psycopg2-binary python-d
 NOTICE: this file will be used to connect to the database and perform CRUD operations on the database
 CRUD = Create, Read, Update, Delete(this will automate the process of adding, retrieving, updating and deleting data)
 '''
+
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL not found in .env file")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
+Base = declarative_base()
